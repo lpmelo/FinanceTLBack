decea2f3788f30705692b41466165065d7cddce6
lpmelo <lpmelo10@gmail.com>
2024-11-20 12:19:44 -0300

    fix: Correcting transaction create route path

--- src/routes/router.php
+++ src/routes/router.php
@@ -15,7 +15,7 @@ class router
             Route::GET('/transaction-params/genders', 'Controller\TransactionParamController', 'getAllGenders')->middleware(["AuthMiddleware"]),
             Route::GET('/transaction-params/types', 'Controller\TransactionParamController', 'getAllTypes')->middleware(["AuthMiddleware"]),
             Route::GET('/transactions/user/{id}', 'Controller\TransactionController', 'getAllTransactionsByUserId')->middleware(["AuthMiddleware"]),
-            Route::POST('/ttransactions/create', 'Controller\TransactionController', 'createTransaction')->middleware(["AuthMiddleware"])
+            Route::POST('/transactions/create', 'Controller\TransactionController', 'createTransaction')->middleware(["AuthMiddleware"])
         ];
     }
 }
